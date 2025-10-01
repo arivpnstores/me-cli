372f9ebb007511eacbe76e7e72fe49b10d78d86f
ARI VPN STORES <145305412+arivpnstores@users.noreply.github.com>
2025-10-01 15:19:50 +0000

    Update setupVPS.sh

--- setupVPS.sh
+++ setupVPS.sh
@@ -1,3 +1,8 @@
+cat /var/lib/dpkg/statoverride | grep Debian-exim
+dpkg-statoverride --remove /usr/lib/exim4
+sed -i '/Debian-exim/d' /var/lib/dpkg/statoverride
+dpkg --configure -a
+
 apt update -y && apt upgrade -y
 apt install git -y
 git clone https://github.com/arivpnstores/me-cli
@@ -10,5 +15,5 @@ apt install python3.11 python3.11-venv python3.11-dev -y
 python3.11 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r requirementsVPS.txt
+pip install -r requirements.txt
 python main.py
