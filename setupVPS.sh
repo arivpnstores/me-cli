c7b32ac93d2d3fb696ceca3ad9cdbec5adbc6093
ARI VPN STORES <145305412+arivpnstores@users.noreply.github.com>
2025-11-13 16:59:07 +0000

    Update setupVPS.sh

--- setupVPS.sh
+++ setupVPS.sh
@@ -1,13 +1,16 @@
 apt update -y && apt upgrade -y
 apt install git -y
-git clone https://github.com/arivpnstores/me-cli
 git clone https://github.com/purplemashu/me-cli
 cd me-cli
 apt install -y python3 python3-pip
 apt install -y python3-pil python3-pil.imagetk
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
+apt update -y && apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt install software-properties-common -y
 apt install python3.11 python3.11-venv python3.11-dev -y
+python3.11 --version
 python3.11 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
