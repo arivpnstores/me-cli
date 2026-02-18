6cfd8119ed57104fcea13338f43272a256b6825f
PT RAJA SERVER PREMIUM <145305412+arivpnstores@users.noreply.github.com>
2026-02-19 02:50:34 +0700

    Update setupVPS.sh

--- setupVPS.sh
+++ setupVPS.sh
@@ -50,8 +50,8 @@ pip install --upgrade pip
 # Jika ada versi requests==2.32.5 di requirements.txt, ganti dulu
 sed -i 's/requests==2.32.5/requests==2.32.4/' requirements.txt
 
-pip install -r requirements.txt
-
+#pip install -r requirements.txt
+bash setup.sh 
 # Jalankan main.py
 echo "▶️ Menjalankan main.py ..."
 python3.11 main.py
