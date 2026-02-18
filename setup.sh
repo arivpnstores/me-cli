7dac1f45f32eaa19670413bc8dc7e341141b0c65
PT RAJA SERVER PREMIUM <145305412+arivpnstores@users.noreply.github.com>
2026-02-19 02:22:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pkg update && pkg upgrade -y
 pkg install git -y
 pkg install python -y
 pkg install python-pillow -y
-git clone https://github.com/purplemashu/me-cli-sunset
+git clone https://github.com/arivpnstores/me-cli-sunset
 cd me-cli-sunset
 pip install -r requirements.txt
 python main.py
