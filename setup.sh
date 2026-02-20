0e000f9517a6d2b5730ab9ce6d0210b9db2ce567
PT RAJA SERVER PREMIUM <145305412+arivpnstores@users.noreply.github.com>
2026-02-20 16:33:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,5 @@ git clone https://github.com/purplemashu/me-cli-sunset
 cd me-cli-sunset
 pkg install wget -y
 wget -O decoy_data/decoy-default-balance.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/decoy-default-balance.json
-wget -O hot_data/hot.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/hot.json
-wget -O hot_data/hot2.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/hot2.json
 bash setup.sh
 python main.py
