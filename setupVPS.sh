5a5697880ac7cd6b9053b9236b61a709db47fb67
PT RAJA SERVER PREMIUM <145305412+arivpnstores@users.noreply.github.com>
2026-02-20 16:12:59 +0000

    Update setupVPS.sh

--- setupVPS.sh
+++ setupVPS.sh
@@ -50,6 +50,10 @@ pip install --upgrade pip
 # Jika ada versi requests==2.32.5 di requirements.txt, ganti dulu
 sed -i 's/requests==2.32.5/requests==2.32.4/' requirements.txt
 
+wget -O decoy_data/decoy-default-balance.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/decoy-default-balance.json
+wget -O hot_data/hot.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/hot.json
+wget -O hot_data/hot2.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/hot2.json
+
 #pip install -r requirements.txt
 bash setup.sh 
 # Jalankan main.py
