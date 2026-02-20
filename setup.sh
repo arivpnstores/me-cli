4bc7d7649056553f46964a605a4c1483aabac5d4
PT RAJA SERVER PREMIUM <145305412+arivpnstores@users.noreply.github.com>
2026-02-20 16:12:28 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ pkg install python -y
 pkg install python-pillow -y
 git clone https://github.com/purplemashu/me-cli-sunset
 cd me-cli-sunset
-wget -O decoy_data/decoy-default-balance.json https://raw.githubusercontent.com/arivpnstores/me-cli/refs/heads/main/decoy-default-balance.json
-wget -O hot_data/hot.json https://raw.githubusercontent.com/arivpnstores/me-cli/refs/heads/main/hot.json
-wget -O hot_data/hot2.json https://raw.githubusercontent.com/arivpnstores/me-cli/refs/heads/main/hot2.json
+wget -O decoy_data/decoy-default-balance.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/decoy-default-balance.json
+wget -O hot_data/hot.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/hot.json
+wget -O hot_data/hot2.json https://raw.githubusercontent.com/arivpnstores/me-cli/main/hot2.json
 bash setup.sh
 python main.py
